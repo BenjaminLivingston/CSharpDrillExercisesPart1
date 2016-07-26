f2f9a95b659c5741cf15c7242dccbe3632a38f98
Benjamin <benjaminlivingston@gmail.com>
2016-07-26 11:09:49 -0700

    Entered simple console functionality.

--- Learning_C_Sharp-Console_Application/Program.cs
+++ Learning_C_Sharp-Console_Application/Program.cs
@@ -4,12 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#region Public methods
+
+#endregion Public methods
+
 namespace Learning_C_Sharp_Console_Application
 {
     class Program
     {
+        // This is where the program starts.
         static void Main(string[] args)
         {
+            // Prompt user to enter a name
+            Console.WriteLine("Enter your name, please: ");
+
+            // Now read the name entered
+            string name = Console.ReadLine();
+
+            // Greet the user with the name that was entered.
+            Console.WriteLine("Hellow, " + name);
+
+            // Wait for user to acknowledge the results.
+            Console.WriteLine("Press Enter to terminate...");
+            Console.Read();
         }
     }
 }
